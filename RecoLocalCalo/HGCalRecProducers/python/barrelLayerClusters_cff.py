0503ac99278711f343077b2b12684d741b77ec5f
brusale <brusale11@gmail.com>
2023-03-20 16:46:56 +0100

    add single producer for barrel layer clusters

---
+++ RecoLocalCalo/HGCalRecProducers/python/barrelLayerClusters_cff.py
@@ -0,0 +1,14 @@
+import FWCore.ParameterSet.Config as cms
+
+from RecoLocalCalo.HGCalRecProducers.barrelLayerClusters_cfi import barrelLayerClusters 
+
+barrelLayerClusters.ebplugin.maxLayerIndex = cms.int32(0)
+barrelLayerClusters.ebplugin.rhoc = cms.double(30)
+
+barrelLayerClusters.hbplugin.maxLayerIndex = cms.int32(3)
+barrelLayerClusters.hbplugin.rhoc = cms.double(30)
+
+barrelLayerClusters.hoplugin.maxLayerIndex = cms.int32(0)
+barrelLayerClusters.hoplugin.rhoc = cms.double(30)
+
+
