c789d7607cc2584b9ce5fd69368c2fc19818eeec
brusale <brusale11@gmail.com>
2023-07-06 21:53:51 +0200

    add function to retrieve density information

--- RecoHGCal/TICL/plugins/LayerClustersDumper.cc
+++ RecoHGCal/TICL/plugins/LayerClustersDumper.cc
@@ -30,6 +30,7 @@
 #include "DataFormats/Math/interface/Point3D.h"
 #include "DataFormats/GeometrySurface/interface/BoundDisk.h"
 #include "DataFormats/HGCalReco/interface/Common.h"
+#include "DataFormats/Math/interface/deltaPhi.h"
 #include "SimDataFormats/CaloAnalysis/interface/CaloParticle.h"
 #include "SimDataFormats/CaloAnalysis/interface/SimCluster.h"
 
@@ -65,6 +66,8 @@ public:
   static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
   typedef math::XYZVector Vector;
   typedef std::vector<double> Vec;
+  typedef std::vector<std::pair<uint32_t, float>> hits_and_floats;
+  typedef std::vector<std::pair<DetId, float>> ids_and_floats;
 
 private:
   void beginJob() override;
@@ -88,9 +91,14 @@ private:
   const edm::EDGetTokenT<hgcal::RecoToSimCollection> recotosim_token_;
 
   //Geometry
-  //const edm::ESGetTken<CaloGeometry, CaloGeometryRecord> geometry_token_;
+  edm::ESGetToken<CaloGeometry, CaloGeometryRecord> geometry_token_;
   
   void clearVariables();
+
+  template<typename T, typename V>
+  void addHitsAndDensities(V& hits_and_densities, T& hits_and_fractions, const CaloGeometry* geom, float totalEnergy);
+  
+  float distance(GlobalPoint point1, GlobalPoint point2);
   unsigned int event_index;
   TTree* tree_;
 
@@ -100,10 +108,16 @@ private:
   std::vector<float> layercluster_energy;
   std::vector<float> layercluster_eta;
   std::vector<float> layercluster_phi;
+  std::vector<float> layercluster_hit_eta;
+  std::vector<float> layercluster_hit_phi;
+  std::vector<float> layercluster_hit_density;
 
   std::vector<float> caloparticle_energy;
   std::vector<float> caloparticle_eta;
   std::vector<float> caloparticle_phi;
+  std::vector<float> caloparticle_hit_eta;
+  std::vector<float> caloparticle_hit_phi;
+  std::vector<float> caloparticle_hit_density;
 
   std::vector<std::vector<float>> cp2lc_score;
   std::vector<std::vector<float>> lc2cp_score;
@@ -125,11 +139,45 @@ void LayerClusterDumper::clearVariables() {
   lc2cp_score.clear();
 }
 
+float LayerClusterDumper::distance (GlobalPoint point1, GlobalPoint point2) {
+  float deltaEta = point1.eta() - point2.eta();
+  auto o2pi = 1./(2*M_PI);
+  float deltaPhi = point1.phi() - point2.phi();
+  if (std::abs(deltaPhi) > M_PI) {
+    auto n = std::round(deltaPhi*o2pi);
+    deltaPhi = deltaPhi - n*M_PI;
+  }
+  return std::sqrt(deltaEta*deltaEta + deltaPhi*deltaPhi);
+}
+
+template<typename T, typename V>
+void LayerClusterDumper::addHitsAndDensities(V& hits_and_densities, T& hits_and_fractions, const CaloGeometry* geom, float totalEnergy) {
+  for (const auto& hit_and_fraction : hits_and_fractions) {
+    auto hit = hit_and_fraction.first;
+    float fraction = hit_and_fraction.second;
+    GlobalPoint position = geom->getPosition(hit);
+    float density = fraction;
+    for (const auto& other_hit_and_fraction : hits_and_fractions) {
+      auto other_hit = other_hit_and_fraction.first;
+      if (other_hit == hit ) continue;
+      float other_fraction = other_hit_and_fraction.second;
+      GlobalPoint other_position = geom->getPosition(other_hit);
+      if (distance(position, other_position) < 1.5*0.0175)
+	density += 0.5*other_fraction;
+    }
+    density *= totalEnergy;
+    hits_and_densities.push_back(std::make_pair(hit, density));
+ }
+}
+
 LayerClusterDumper::LayerClusterDumper(const edm::ParameterSet& ps)
   : layer_clusters_token_(consumes<std::vector<reco::CaloCluster>>(ps.getParameter<edm::InputTag>("layerclusters"))),
     caloparticles_token_(consumes<std::vector<CaloParticle>>(ps.getParameter<edm::InputTag>("caloparticles"))),
     simtoreco_token_(consumes<hgcal::SimToRecoCollection>(ps.getParameter<edm::InputTag>("simToRecoCollection"))),
-    recotosim_token_(consumes<hgcal::RecoToSimCollection>(ps.getParameter<edm::InputTag>("recoToSimCollection"))) {};
+    recotosim_token_(consumes<hgcal::RecoToSimCollection>(ps.getParameter<edm::InputTag>("recoToSimCollection"))) {
+      
+      geometry_token_ = esConsumes<CaloGeometry, CaloGeometryRecord>();
+};
 
 LayerClusterDumper::~LayerClusterDumper() { clearVariables(); };
 
@@ -142,10 +190,16 @@ void LayerClusterDumper::beginJob() {
   layercluster_tree_->Branch("layerClusterEnergy", &layercluster_energy);
   layercluster_tree_->Branch("layerClusterEta", &layercluster_eta);
   layercluster_tree_->Branch("layerClusterPhi", &layercluster_phi);
-  
+  layercluster_tree_->Branch("layerClusterHitEta", &layercluster_hit_eta);
+  layercluster_tree_->Branch("layerClusterHitPhi", &layercluster_hit_phi);
+  layercluster_tree_->Branch("layerClusterHitDensity", &layercluster_hit_density);
+
   caloparticle_tree_->Branch("caloParticleEnergy", &caloparticle_energy);
   caloparticle_tree_->Branch("caloParticleEta", &caloparticle_eta);
   caloparticle_tree_->Branch("caloParticlePhi", &caloparticle_phi);
+  caloparticle_tree_->Branch("caloParticleHitEta", &caloparticle_hit_eta);
+  caloparticle_tree_->Branch("caloParticleHitPhi", &caloparticle_hit_phi);
+  caloparticle_tree_->Branch("caloParticleHitDensity", &caloparticle_hit_density);
 
   association_tree_->Branch("simToRecoAssociation", &cp2lc_score);
   association_tree_->Branch("recoToSimAssociation", &lc2cp_score);
@@ -156,6 +210,8 @@ void LayerClusterDumper::beginJob() {
 void LayerClusterDumper::analyze(const edm::Event& event, const edm::EventSetup& setup) {
   event_index++;
   clearVariables();
+
+  const CaloGeometry* geom = &(setup.getData(geometry_token_));
 
   edm::Handle<std::vector<reco::CaloCluster>> layer_clusters_h;
   event.getByToken(layer_clusters_token_, layer_clusters_h);
@@ -181,13 +237,39 @@ void LayerClusterDumper::analyze(const edm::Event& event, const edm::EventSetup&
     layercluster_energy.push_back(lc_iterator->energy());
     layercluster_eta.push_back(lc_iterator->eta());
     layercluster_phi.push_back(lc_iterator->phi());
+    ids_and_floats hits_and_fractions = lc_iterator->hitsAndFractions();
+    float lc_energy = lc_iterator->energy();
+    ids_and_floats hits_and_densities;
+    hits_and_densities.resize(hits_and_fractions.size());
+    addHitsAndDensities(hits_and_densities, hits_and_fractions, geom, lc_energy);
+    for (const auto& hit_and_density : hits_and_densities) {
+      uint32_t hit = hit_and_density.first;
+      float density = hit_and_density.second;
+      GlobalPoint position = geom->getPosition(hit);
+      layercluster_hit_eta.push_back(position.eta());
+      layercluster_hit_phi.push_back(position.phi());
+      layercluster_hit_density.push_back(density);
+    }
   }
 
   for (auto cp_iterator = caloparticles.begin(); cp_iterator != caloparticles.end(); ++cp_iterator) {
     caloparticle_energy.push_back(cp_iterator->energy());
     caloparticle_eta.push_back(cp_iterator->eta());
     caloparticle_phi.push_back(cp_iterator->phi());
-  }
+    const hits_and_floats hits_and_fractions = cp_iterator->hits_and_fractions();
+    float cp_energy = cp_iterator->energy();
+    hits_and_floats hits_and_densities;
+    hits_and_densities.resize(hits_and_fractions.size());
+    addHitsAndDensities(hits_and_densities,hits_and_fractions, geom, cp_energy);
+    for (const auto& hit_and_density : hits_and_densities) {
+      uint32_t hit = hit_and_density.first;
+      float density = hit_and_density.second;
+      GlobalPoint position = geom->getPosition(hit);
+      caloparticle_hit_eta.push_back(position.eta());
+      caloparticle_hit_phi.push_back(position.phi());
+      caloparticle_hit_density.push_back(density);
+    }
+}
 
   lc2cp_score.resize(layer_clusters.size());
   for (unsigned int lcId = 0; lcId < layer_clusters.size(); ++lcId) {
@@ -224,8 +306,8 @@ void LayerClusterDumper::fillDescriptions(edm::ConfigurationDescriptions& descri
   edm::ParameterSetDescription desc;
   desc.add<edm::InputTag>("layerclusters", edm::InputTag("barrelLayerClusters"));
   desc.add<edm::InputTag>("caloparticles", edm::InputTag("mix", "MergedCaloTruth"));
-  desc.add<edm::InputTag>("simToRecoCollection", edm::InputTag("layerClusterCaloParticleAssociationProducer"));
-  desc.add<edm::InputTag>("recoToSimCollection", edm::InputTag("layerClusterCaloParticleAssociationProducer"));
+  desc.add<edm::InputTag>("simToRecoCollection", edm::InputTag("barrelLayerClusterCaloParticleAssociationProducer"));
+  desc.add<edm::InputTag>("recoToSimCollection", edm::InputTag("barrelLayerClusterCaloParticleAssociationProducer"));
 
   descriptions.add("layerClusterDumper", desc);
 }
