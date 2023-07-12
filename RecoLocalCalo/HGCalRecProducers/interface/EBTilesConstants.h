9e9dff8991c80dfa58d29f0e3bba13bbd7ce95a3
brusale <brusale11@gmail.com>
2023-07-12 16:05:45 +0200

    re-fix ECAL shower shape

--- RecoLocalCalo/HGCalRecProducers/interface/EBTilesConstants.h
+++ RecoLocalCalo/HGCalRecProducers/interface/EBTilesConstants.h
@@ -20,7 +20,7 @@ struct EBTilesConstants {
   static constexpr int nTiles = nColumns * nRows;
   static constexpr float cellWidthEta = 0.0175f;
   static constexpr float cellWidthPhi = 0.0175f;
-  static constexpr float showerSigma = 0.05f; // in unit of xtals
+  static constexpr float showerSigma = 0.5f; // in unit of xtals
 };
 
 #endif
