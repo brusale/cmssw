29fa220aec6dfa4cab4faa487364946fea5c5348
brusale <brusale11@gmail.com>
2023-08-22 14:18:31 +0200

    add cut on PCaloHits in CaloParticle

--- SimGeneral/CaloAnalysis/plugins/CaloTruthAccumulator.cc
+++ SimGeneral/CaloAnalysis/plugins/CaloTruthAccumulator.cc
@@ -497,7 +497,8 @@ void CaloTruthAccumulator::finalizeEvent(edm::Event &event, edm::EventSetup cons
       sc.clearHitsAndFractions();
       sc.clearHitsEnergy();
       for (auto &hAndE : hitsAndEnergies) {
-        const float totalenergy = m_detIdToTotalSimEnergy[hAndE.first];
+	if (hAndE.second < 1e-2) continue;
+	const float totalenergy = m_detIdToTotalSimEnergy[hAndE.first];
         float fraction = 0.;
         if (totalenergy > 0)
           fraction = hAndE.second / totalenergy;
