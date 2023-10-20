a7f0d27672b82bbda727e5fec16b0940b6f3ad2d
brusale <brusale11@gmail.com>
2023-10-20 08:54:31 +0200

    add classes files for different backends

---
+++ DataFormats/CaloRecHit/src/alpaka/classes_cuda.h
@@ -0,0 +1,12 @@
+#ifndef DataFormats_CaloRecHit_alpaka_classes_cuda_h
+#define DataFormats_CaloRecHit_alpaka_classes_cuda_h
+
+#include "DataFormats/Common/interface/Wrapper.h"
+
+#include "DataFormats/CaloRecHit/interface/LayerClusters.h"
+#include "DataFormats/CaloRecHit/interface/LayerClustersHostCollection.h"
+#include "DataFormats/CaloRecHit/interface/CaloClustersHostCollection.h"
+#include "DataFormats/CaloRecHit/interface/alpaka/LayerClustersDeviceCollection.h"
+#include "DataFormats/CaloRecHit/interface/alpaka/CaloClustersDeviceCollection.h"
+
+#endif
---
+++ DataFormats/CaloRecHit/src/alpaka/classes_rocm.h
@@ -0,0 +1,12 @@
+#ifndef DataFormats_CaloRecHit_alpaka_rocm_h
+#define DataFormats_CaloRecHit_alpaka_rocm_h
+
+#include "DataFormats/Common/interface/Wrapper.h"
+
+#include "DataFormats/CaloRecHit/interface/LayerClusters.h"
+#include "DataFormats/CaloRecHit/interface/LayerClustersHostCollection.h"
+#include "DataFormats/CaloRecHit/interface/CaloClustersHostCollection.h"
+#include "DataFormats/CaloRecHit/interface/alpaka/LayerClustersDeviceCollection.h"
+#include "DataFormats/CaloRecHit/interface/alpaka/CaloClustersDeviceCollection.h"
+
+#endif
