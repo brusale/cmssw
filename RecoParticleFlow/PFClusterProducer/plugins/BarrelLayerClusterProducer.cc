0055e22866dd9084ca1c59908e78219b9ec8a040
brusale <brusale11@gmail.com>
2024-07-08 11:32:48 +0200

    split layercluster between ecal and hcal

--- RecoParticleFlow/PFClusterProducer/plugins/BarrelLayerClusterProducer.cc
+++ RecoParticleFlow/PFClusterProducer/plugins/BarrelLayerClusterProducer.cc
@@ -94,11 +94,13 @@ BarrelLayerClusterProducer::BarrelLayerClusterProducer(const edm::ParameterSet& 
 
   timeResolutionCalc_ = std::make_unique<CaloRecHitResolutionProvider>(ps.getParameterSet("timeResolutionCalc"));
   //produces<std::vector<float>>("InitialLayerClustersMask");
-  produces<std::vector<reco::BasicCluster>>();
+  produces<std::vector<reco::BasicCluster>>("ecalLayerClusters");
+  produces<std::vector<reco::BasicCluster>>("hcalLayerClusters");
   //density
   //produces<Density>();
   //time for layer clusters
-  produces<edm::ValueMap<std::pair<float, float>>>(timeClname);
+  produces<edm::ValueMap<std::pair<float, float>>>(timeClname+"_ecal");
+  produces<edm::ValueMap<std::pair<float, float>>>(timeClname+"_hcal");
 }
 
 void BarrelLayerClusterProducer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
@@ -185,30 +187,35 @@ void BarrelLayerClusterProducer::produce(edm::Event& evt, const edm::EventSetup&
   //*hoclusters = hoalgo->getClusters(false);
   std::vector<reco::BasicCluster> hoclusters = hoalgo->getClusters(false);
   
-  std::unique_ptr<std::vector<reco::BasicCluster>> clusters(new std::vector<reco::BasicCluster>);
-  (*clusters).reserve(ebclusters.size() + hbclusters.size()); // + hoclusters.size());
-  (*clusters).insert((*clusters).end(), ebclusters.begin(), ebclusters.end());
-  (*clusters).insert((*clusters).end(), hbclusters.begin(), hbclusters.end());
-  (*clusters).insert((*clusters).end(), hoclusters.begin(), hoclusters.end());
-  
-  auto clusterHandle = evt.put(std::move(clusters));
+  std::unique_ptr<std::vector<reco::BasicCluster>> clusters_ecal(new std::vector<reco::BasicCluster>);
+  (*clusters_ecal).reserve(ebclusters.size());
+  (*clusters_ecal).insert((*clusters_ecal).end(), ebclusters.begin(), ebclusters.end());
 
+  std::unique_ptr<std::vector<reco::BasicCluster>> clusters_hcal(new std::vector<reco::BasicCluster>);
+  (*clusters_hcal).reserve(hbclusters.size());
+  (*clusters_hcal).insert((*clusters_hcal).end(), hbclusters.begin(), hbclusters.end());
+  //(*clusters).insert((*clusters).end(), hoclusters.begin(), hoclusters.end());
+  
+  auto clusterHandleEcal = evt.put(std::move(clusters_ecal), "ecalLayerClusters");
+  auto clusterHandleHcal = evt.put(std::move(clusters_hcal), "hcalLayerClusters");
   //Keep the density
   /**density = algo->getDensity();
   evt.put(std::move(density));*/
 
-  edm::PtrVector<reco::BasicCluster> clusterPtrs;  //, clusterPtrsSharing;
+  edm::PtrVector<reco::BasicCluster> clusterEcalPtrs;  //, clusterPtrsSharing;
+  edm::PtrVector<reco::BasicCluster> clusterHcalPtrs;  //, clusterPtrsSharing;
+  
+  std::vector<std::pair<float, float>> times_ecal, times_hcal;
+  times_ecal.reserve(clusterHandleEcal->size());
+  times_hcal.reserve(clusterHandleHcal->size());
 
-  std::vector<std::pair<float, float>> times;
-  times.reserve(clusterHandle->size());
-
-  for (unsigned i = 0; i < clusterHandle->size(); ++i) {
-    edm::Ptr<reco::BasicCluster> ptr(clusterHandle, i);
-    clusterPtrs.push_back(ptr);
+  for (unsigned i = 0; i < clusterHandleEcal->size(); ++i) {
+    edm::Ptr<reco::BasicCluster> ptr(clusterHandleEcal, i);
+    clusterEcalPtrs.push_back(ptr);
 
     std::pair<float, float> timeCl(-99., -1.);
 
-    const reco::CaloCluster& sCl = (*clusterHandle)[i];
+    const reco::CaloCluster& sCl = (*clusterHandleEcal)[i];
     if (sCl.size() >= nHitsTime) {
       std::vector<float> timeClhits;
       std::vector<float> timeErrorClhits;
@@ -230,18 +237,56 @@ void BarrelLayerClusterProducer::produce(edm::Event& evt, const edm::EventSetup&
       hgcalsimclustertime::ComputeClusterTime timeEstimator;
       timeCl = timeEstimator.fixSizeHighestDensity(timeClhits, timeErrorClhits, nHitsTime);
     }
-    times.push_back(timeCl);
+    times_ecal.push_back(timeCl);
   }
+
+  for (unsigned i = 0; i < clusterHandleHcal->size(); ++i) {
+    edm::Ptr<reco::BasicCluster> ptr(clusterHandleHcal, i);
+    clusterHcalPtrs.push_back(ptr);
+
+    std::pair<float, float> timeCl(-99., -1.);
+
+    const reco::CaloCluster& sCl = (*clusterHandleHcal)[i];
+    if (sCl.size() >= nHitsTime) {
+      std::vector<float> timeClhits;
+      std::vector<float> timeErrorClhits;
+
+      for (auto const& hit : sCl.hitsAndFractions()) {
+        auto finder = hitmap.find(hit.first);
+        if (finder == hitmap.end())
+          continue;
+
+        //time is computed wrt  0-25ns + offset and set to -1 if no time
+        const reco::PFRecHit* rechit = finder->second;
+        float rhTimeE = timeResolutionCalc_->timeResolution2(rechit->energy());
+        //check on timeError to exclude scintillator
+        if (rhTimeE < 0.)
+          continue;
+        timeClhits.push_back(rechit->time());
+        timeErrorClhits.push_back(1. / rhTimeE);
+      }
+      hgcalsimclustertime::ComputeClusterTime timeEstimator;
+      timeCl = timeEstimator.fixSizeHighestDensity(timeClhits, timeErrorClhits, nHitsTime);
+    }
+    times_hcal.push_back(timeCl);
+  }
+
   //std::unique_ptr<std::vector<float>> layerClustersMask(new std::vector<float>);
   //layerClustersMask->resize(clusterHandle->size(), 1.0);
   //evt.put(std::move(layerClustersMask), "InitialLayerClustersMask");
 
-  auto timeCl = std::make_unique<edm::ValueMap<std::pair<float, float>>>();
-  edm::ValueMap<std::pair<float, float>>::Filler filler(*timeCl);
-  filler.insert(clusterHandle, times.begin(), times.end());
-  filler.fill();
-  evt.put(std::move(timeCl), timeClname);
+  auto timeCl_ecal = std::make_unique<edm::ValueMap<std::pair<float, float>>>();
+  edm::ValueMap<std::pair<float, float>>::Filler filler_ecal(*timeCl_ecal);
+  filler_ecal.insert(clusterHandleEcal, times_ecal.begin(), times_ecal.end());
+  filler_ecal.fill();
+  evt.put(std::move(timeCl_ecal), timeClname+"_ecal");
 
+   auto timeCl_hcal = std::make_unique<edm::ValueMap<std::pair<float, float>>>();
+   edm::ValueMap<std::pair<float, float>>::Filler filler_hcal(*timeCl_hcal);
+   filler_hcal.insert(clusterHandleHcal, times_hcal.begin(), times_hcal.end());
+   filler_hcal.fill();
+   evt.put(std::move(timeCl_hcal), timeClname+"_hcal");
+   
   /*if (doSharing) {
     for (unsigned i = 0; i < clusterHandleSharing->size(); ++i) {
       edm::Ptr<reco::BasicCluster> ptr(clusterHandleSharing, i);
