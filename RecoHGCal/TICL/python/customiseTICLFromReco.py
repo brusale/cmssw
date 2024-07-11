5ed372559caed65a42f2f741a05e78a4d1051e0c
brusale <brusale11@gmail.com>
2024-07-11 09:16:37 +0200

    add SimTracksters in schedule

--- RecoHGCal/TICL/python/customiseTICLFromReco.py
+++ RecoHGCal/TICL/python/customiseTICLFromReco.py
@@ -108,7 +108,7 @@ def customiseTICLBarrelFromReco(process):
         patternRecognitionBy = "FastJet",
         doRegression = cms.bool(False),
         pluginPatternRecognitionByFastJet = dict (
-            antikt_radius = 10,
+            antikt_radius = 0.1,
             minNumLayerCluster = 0,
             algo_verbosity = 2
         )
@@ -123,15 +123,18 @@ def customiseTICLBarrelFromReco(process):
         patternRecognitionBy = "FastJet", #"CLUE2D",
         doRegression = cms.bool(False),
         pluginPatternRecognitionByFastJet = dict (
-            antikt_radius = 10,
+            antikt_radius = 0.1,
             minNumLayerCluster = 0,
             algo_verbosity = 2
         )
     )
-    
+   
+    process.ticlSimTracksters.layer_clusters = cms.InputTag("barrelLayerClusters", "hcalLayerClusters") 
+    process.ticlSimTracksters.filtered_mask = cms.InputTag("barrelLayerClusters:InitialLayerClustersMaskHCAL")
+    process.ticlSimTracksters.time_layerclusters = cms.InputTag("barrelLayerClusters:timeLayerClusterHcal")
     process.hcalPatternRecognitionTask = cms.Task(process.barrelHcalPatternRecognition) # + process.barrelEcalPatternRecognition
     process.ticlLayerTileProducer.detector = cms.string('HCAL')
-    process.TICLBarrel = cms.Path(process.ticlLayerTileProducer + process.ticlSeedingGlobal, process.barrelLayerClustersTask, process.hcalPatternRecognitionTask)
+    process.TICLBarrel = cms.Path(process.ticlSimTracksters + process.ticlLayerTileProducer + process.ticlSeedingGlobal, process.barrelLayerClustersTask, process.hcalPatternRecognitionTask)
 
     # We want to run CLUE on not -cleaned Rechit collections
     process.recHitMapProducer.EBInput = cms.InputTag("particleFlowRecHitECAL")
@@ -208,7 +211,7 @@ def customiseTICLBarrelFromReco(process):
       eventProducts = cms.untracked.vstring("barrelLayerClusterSimClusterAssociationProducerPFCluster")
     )
     process.consumer6 = cms.EDAnalyzer("GenericConsumer",
-      eventProducts = cms.untracked.vstring("barrelHcalPatternRecognition")
+      eventProducts = cms.untracked.vstring(["barrelHcalPatternRecognition", "ticlSimTracksters"])
     )
     process.FEVTDEBUGHLToutput_step = cms.EndPath(process.FEVTDEBUGHLToutput+
                                                   process.consumer + process.consumer2 + process.consumer3 + process.consumer4 + process.consumer5+process.consumer6+
