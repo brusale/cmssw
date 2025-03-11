fa25b0e1cdf9293acc60e9dbce7dff1b98d9c2c2
brusale <brusale11@gmail.com>
2025-03-11 17:45:40 +0100

    integrate Barrel LCs in HLT sequences
    HGCalMergeLayerClusters now includes the LayerClusters from the barrel
    in the HLT sequences as well.

--- HLTrigger/Configuration/python/HLT_75e33/sequences/HLTPhoton187UnseededSequence_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/sequences/HLTPhoton187UnseededSequence_cfi.py
@@ -8,6 +8,7 @@ from ..modules.l1tTkEmSingle51Filter_cfi import *
 from ..modules.hltEgammaCandidatesUnseeded_cfi import *
 from ..modules.hltEgammaHGCALIDVarsUnseeded_cfi import *
 from ..modules.hltEgammaHoverEUnseeded_cfi import *
+from ..modules.hltParticleFlowRecHitHBHE_cfi import *
 
 from ..sequences.HLTDoFullUnpackingEgammaEcalSequence_cfi import *
 from ..sequences.HLTEGammaDoLocalHcalSequence_cfi import *
@@ -18,6 +19,7 @@ from ..sequences.HLTFastJetForEgammaSequence_cfi import *
 
 HLTPhoton187UnseededSequence = cms.Sequence(HLTL1Sequence
                                             +l1tTkEmSingle51Filter
+                                            +hltParticleFlowRecHitHBHE
                                             +HLTDoFullUnpackingEgammaEcalSequence
                                             +HLTPFClusteringForEgammaUnseededSequence
                                             +HLTHgcalTiclPFClusteringForEgammaUnseededSequence
